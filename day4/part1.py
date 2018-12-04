be00effd1e41f407a973387ac644b8024b4972ad
tremorai <tremor.liad@gmail.com>
2018-12-04 16:18:56 -0700

    solved part 1 and 2 of day4

---
+++ day4/part1.py
@@ -0,0 +1,50 @@
+import re
+from collections import Counter
+file_input = list(map(str.strip, open("day4/input.txt").readlines()))
+# [1518-05-21 23:59] Guard #863 begins shift  year month day
+re_log = re.compile(r"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (.+)")
+guard_log = {}
+log_sorted = []
+guard_id = -1
+guard_sleep = -1
+guard_length = 0
+max_guard_id = 0
+HELP = 0
+
+
+def parse_line(line):
+    year, month, day, hour, minuet, text = re_log.search(line).groups()
+    guard_num = re.search(r'Guard #(\d+)', line)
+    if guard_num:
+        guard_num = int(guard_num[1])
+    return int(year), int(month), int(day), int(hour), int(minuet), text, guard_num
+
+
+for line in file_input:
+    year, month, day, hour, minuet, text, guard_num = parse_line(line)
+    log_sorted.append((year, month, day, hour, minuet, text, guard_num))
+
+    if guard_num:
+        if guard_num not in guard_log:
+            guard_log[guard_num] = []
+log_sorted.sort(key=lambda x: x[:5])
+for thing in log_sorted:
+    if thing[6]:
+        guard_id = thing[6]
+        print(f"{guard_id} is now on duty")
+    if thing[5] == "falls asleep":
+        print(f"{guard_id} has fallen asleepy pie")
+        guard_sleep = int(thing[4])
+    if thing[5] == "wakes up" or thing[6] and guard_sleep != -1:
+        for i in range(guard_sleep, thing[4]):
+            guard_log[guard_id].append(i)
+        print(f"{guard_id} has awoken!")
+        guard_sleep = -1
+for r in guard_log:
+    if len(guard_log[r]) > guard_length:
+        guard_length = len(guard_log[r])
+        max_guard_id = r
+THING = Counter(guard_log[max_guard_id]).most_common(1)[0][0]
+print(
+    f"{max_guard_id} has a value {guard_length} minuet spent most {THING}")
+print(f"HELP ME {max_guard_id * THING}")
